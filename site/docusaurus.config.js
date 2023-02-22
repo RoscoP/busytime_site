64eb5a55d79c08369cb9b3f254451b1b6d4dd9b3
Ralph Lewis <RoscoP@users.noreply.github.com>
2023-02-22 14:07:33 -0800

    Add google analytics

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -46,6 +46,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-TGQCDHSP9R',
+          anonymizeIP: false,
+        },
       }),
     ],
   ],
